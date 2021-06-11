93de437affb3d101dfc2d1c248aa201311ae30b2
micessien <micaeldie@gmail.com>
2021-06-11 03:55:07 +0000

    Fix Remote Debugger is in a Background

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 // Components
 import Navigation from "./Navigation/Navigation";
 
 export default function App() {
+  // Ignore log Remote debugger notification by message:
+  LogBox.ignoreLogs(['Remote debugger']);
+
+  // Ignore all log notifications: This is useful when giving product demos, for example
+  // LogBox.ignoreAllLogs();
+
   return (
     <Navigation />
   );
