b80957e8855e4ed0b2e9359e022060f4362d2918
micessien <micaeldie@gmail.com>
2021-07-15 13:17:03 +0000

    Add Animated to the filmItem component and change the componenet to class

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,35 +1,56 @@
 import React from 'react'
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, Image, TouchableOpacity, Animated, Dimensions } from 'react-native'
 import DetailsFilm from './filmItem/DetailsFilm'
 import { getImageFromApi } from '../API/TMDBApi'
 
-function FilmItem(props) {
-        const { film, displayDetailForFilm } = props;
+class FilmItem extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        positionLeft: new Animated.Value(Dimensions.get('window').width)
+      }
+    }
 
-        const _displayFavoriteImage = () => {
-          if (props.isFilmFavorite) {
-            // Si la props isFilmFavorite vaut true, on affiche le 🖤
-            return (
-              <Image
-                style={styles.favorite_image}
-                source={require('../Images/ic_favorite.png')}
-              />
-            )
-          }
-        }
+    componentDidMount() {
+      Animated.spring(
+        this.state.positionLeft,
+        {
+          toValue: 0,
+          duration: 500,
+          useNativeDriver: true, // <-- Add this
+        }).start()
+    }
 
+    _displayFavoriteImage() {
+      if (this.props.isFilmFavorite) {
+        // Si la props isFilmFavorite vaut true, on affiche le 🖤
         return (
-            <TouchableOpacity
-              onPress={() => displayDetailForFilm(film.id)}
-              style={styles.main_container}>
-                <Image
-                style={styles.image}
-                source={{uri: getImageFromApi(film.poster_path)}}
-                />
-
-                <DetailsFilm favoriteImage={_displayFavoriteImage()} film={film} />
-            </TouchableOpacity>
+          <Image
+            style={styles.favorite_image}
+            source={require('../Images/ic_favorite.png')}
+          />
         )
+      }
+    }
+
+    render() {
+      const { film, displayDetailForFilm } = this.props;
+
+      return (
+        <Animated.View style={{left: this.state.positionLeft}}>
+          <TouchableOpacity
+            onPress={() => displayDetailForFilm(film.id)}
+            style={styles.main_container}>
+              <Image
+              style={styles.image}
+              source={{uri: getImageFromApi(film.poster_path)}}
+              />
+
+              <DetailsFilm favoriteImage={this._displayFavoriteImage()} film={film} />
+          </TouchableOpacity>
+        </Animated.View>
+      )
+    }
 }
 
 const styles = StyleSheet.create({
