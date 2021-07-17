b24ac234f0ab0ebec978e9624b321d924a30d676
micessien <micaeldie@gmail.com>
2021-07-17 01:22:59 +0000

    Fix les erreur eslint

--- Components/Favorites.js
+++ Components/Favorites.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import { StyleSheet, Text } from 'react-native'
-import FilmList from './FilmList'
-import { connect } from 'react-redux'
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { connect } from 'react-redux';
+import FilmList from "./FilmList";
 
 function Favorites(props) {
-    return (
-      <FilmList
-        films={props.favoritesFilm}
-        navigation={props.navigation}
-        favoriteList={true} // Ici on est bien dans le cas de la liste des films favoris. Ce booléen à true permettra d'empêcher de lancer la recherche de plus de films après un scroll lorsqu'on est sur la vue Favoris.
-      />
-    )
+  return (
+    <FilmList
+      films={props.favoritesFilm}
+      navigation={props.navigation}
+      favoriteList // Ici on est bien dans le cas de la liste des films favoris. Ce booléen à true permettra d'empêcher de lancer la recherche de plus de films après un scroll lorsqu'on est sur la vue Favoris.
+    />
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
-const mapStateToProps = state => {
-  return {
-    favoritesFilm: state.favoritesFilm
-  }
-}
+const mapStateToProps = state => ({
+    favoritesFilm: state.favoritesFilm,
+  });
 
-export default connect(mapStateToProps)(Favorites)
+export default connect(mapStateToProps)(Favorites);
--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,88 +1,112 @@
-import React from 'react'
-import { StyleSheet, Share, View, Text, ActivityIndicator, ScrollView, Image, TouchableOpacity, Platform } from 'react-native'
-import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBApi'
-import moment from 'moment'
-import numeral from 'numeral'
-import { connect } from 'react-redux'
+import React from 'react';
+import {
+  StyleSheet,
+  Share,
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import {getFilmDetailFromApi, getImageFromApi} from '../API/TMDBApi';
+import moment from 'moment';
+import numeral from 'numeral';
+import {connect} from 'react-redux';
 // Components
-import EnlargeShrink from '../Animations/EnlargeShrink'
+import EnlargeShrink from '../Animations/EnlargeShrink';
 
 class FilmDetail extends React.Component {
-
-  static navigationOptions = ({ navigation }) => {
-    const { params } = navigation.state
+  static navigationOptions = ({navigation}) => {
+    const {params} = navigation.state;
     // On accède à la fonction shareFilm et au film via les paramètres qu'on a ajouté à la navigation
     if (params.film != undefined && Platform.OS === 'ios') {
       return {
-          // On a besoin d'afficher une image, il faut donc passe par une Touchable une fois de plus
-          headerRight: <TouchableOpacity
-                          style={styles.share_touchable_headerrightbutton}
-                          onPress={() => params.shareFilm()}>
-                          <Image
-                            style={styles.share_image}
-                            source={require('../Images/ic_share.ios.png')} />
-                        </TouchableOpacity>
-      }
+        // On a besoin d'afficher une image, il faut donc passe par une Touchable une fois de plus
+        headerRight: (
+          <TouchableOpacity
+            style={styles.share_touchable_headerrightbutton}
+            onPress={() => params.shareFilm()}>
+            <Image
+              style={styles.share_image}
+              source={require('../Images/ic_share.ios.png')}
+            />
+          </TouchableOpacity>
+        ),
+      };
     }
-}
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       film: undefined,
-      isLoading: true
-    }
-    this._toggleFavorite = this._toggleFavorite.bind(this)
+      isLoading: true,
+    };
+    this._toggleFavorite = this._toggleFavorite.bind(this);
     // Ne pas oublier de binder la fonction _shareFilm sinon, lorsqu'on va l'appeler depuis le headerRight de la navigation, this.state.film sera undefined et fera planter l'application
-    this._shareFilm = this._shareFilm.bind(this)
+    this._shareFilm = this._shareFilm.bind(this);
   }
 
   componentDidMount() {
-    const favoriteFilmIndex = this.props.favoritesFilm.findIndex(item => item.id === this.props.navigation.state.params.idFilm)
+    const favoriteFilmIndex = this.props.favoritesFilm.findIndex(
+      item => item.id === this.props.navigation.state.params.idFilm,
+    );
     if (favoriteFilmIndex !== -1) {
-      this.setState({
-        film: this.props.favoritesFilm[favoriteFilmIndex]
-      }, () => { this._updateNavigationParams() })
-      return
+      this.setState(
+        {
+          film: this.props.favoritesFilm[favoriteFilmIndex],
+        },
+        () => {
+          this._updateNavigationParams();
+        },
+      );
+      return;
     }
     // Le film n'est pas dans nos favoris, on n'a pas son détail
     // On appelle l'API pour récupérer son détail
-    this.setState({ isLoading: true })
-    getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
-      this.setState({
-        film: data,
-        isLoading: false
-      }, () => { this._updateNavigationParams() })
-    })
+    this.setState({isLoading: true});
+    getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(
+      data => {
+        this.setState(
+          {
+            film: data,
+            isLoading: false,
+          },
+          () => {
+            this._updateNavigationParams();
+          },
+        );
+      },
+    );
   }
 
   _shareFilm() {
-    const { film } = this.state
-    Share.share({ title: film.title, message: film.overview })
+    const {film} = this.state;
+    Share.share({title: film.title, message: film.overview});
   }
 
   // Fonction pour faire passer la fonction _shareFilm et le film aux paramètres de la navigation. Ainsi on aura accès à ces données au moment de définir le headerRight
   _updateNavigationParams() {
     this.props.navigation.setParams({
       shareFilm: this._shareFilm,
-      film: this.state.film
-    })
+      film: this.state.film,
+    });
   }
 
   _displayFloatingActionButton() {
-    const { film } = this.state
+    const {film} = this.state;
     let sourceImage = require('../Images/ic_share.android.png');
 
     if (film != undefined && Platform.OS === 'android') {
       return (
         <TouchableOpacity
-        style={styles.share_touchable_floatingactionbutton}
-        onPress={() => this._shareFilm()}>
-          <Image
-            style={styles.share_image}
-            source={sourceImage}/>
+          style={styles.share_touchable_floatingactionbutton}
+          onPress={() => this._shareFilm()}>
+          <Image style={styles.share_image} source={sourceImage} />
         </TouchableOpacity>
-      )
+      );
     }
   }
 
@@ -90,38 +114,38 @@ class FilmDetail extends React.Component {
     if (this.state.isLoading) {
       return (
         <View style={styles.loading_container}>
-          <ActivityIndicator size='large' />
+          <ActivityIndicator size="large" />
         </View>
-      )
+      );
     }
   }
 
   _toggleFavorite() {
-    const action = { type: "TOGGLE_FAVORITE", value: this.state.film }
-    this.props.dispatch(action)
+    const action = {type: 'TOGGLE_FAVORITE', value: this.state.film};
+    this.props.dispatch(action);
   }
 
   _displayFavoriteImage() {
-    var sourceImage = require('../Images/ic_favorite_border.png')
-    var shouldEnlarge = false // Par défaut, si le film n'est pas en favoris, on veut qu'au clic sur le bouton, celui-ci s'agrandisse => shouldEnlarge à true
-    if (this.props.favoritesFilm.findIndex(item => item.id === this.state.film.id) !== -1) {
-      sourceImage = require('../Images/ic_favorite.png')
-      shouldEnlarge = true // Si le film est dans les favoris, on veut qu'au clic sur le bouton, celui-ci se rétrécisse => shouldEnlarge à false
+    var sourceImage = require('../Images/ic_favorite_border.png');
+    var shouldEnlarge = false; // Par défaut, si le film n'est pas en favoris, on veut qu'au clic sur le bouton, celui-ci s'agrandisse => shouldEnlarge à true
+    if (
+      this.props.favoritesFilm.findIndex(
+        item => item.id === this.state.film.id,
+      ) !== -1
+    ) {
+      sourceImage = require('../Images/ic_favorite.png');
+      shouldEnlarge = true; // Si le film est dans les favoris, on veut qu'au clic sur le bouton, celui-ci se rétrécisse => shouldEnlarge à false
     }
 
     return (
-      <EnlargeShrink
-        shouldEnlarge={shouldEnlarge}>
-        <Image
-          style={styles.favorite_image}
-          source={sourceImage}
-        />
+      <EnlargeShrink shouldEnlarge={shouldEnlarge}>
+        <Image style={styles.favorite_image} source={sourceImage} />
       </EnlargeShrink>
-    )
+    );
   }
 
   _displayFilm() {
-    const { film } = this.state
+    const {film} = this.state;
     if (film != undefined) {
       return (
         <ScrollView style={styles.scrollview_container}>
@@ -134,22 +158,38 @@ class FilmDetail extends React.Component {
             style={styles.favorite_container}
             onPress={() => this._toggleFavorite()}>
             {this._displayFavoriteImage()}
-           </TouchableOpacity>
+          </TouchableOpacity>
           <Text style={styles.description_text}>{film.overview}</Text>
-          <Text style={styles.default_text}>Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}</Text>
-          <Text style={styles.default_text}>Note : {film.vote_average} / 10</Text>
-          <Text style={styles.default_text}>Nombre de votes : {film.vote_count}</Text>
-          <Text style={styles.default_text}>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
-          <Text style={styles.default_text}>Genre(s) : {film.genres.map(function(genre){
-              return genre.name;
-            }).join(" / ")}
+          <Text style={styles.default_text}>
+            Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}
           </Text>
-          <Text style={styles.default_text}>Companie(s) : {film.production_companies.map(function(company){
-              return company.name;
-            }).join(" / ")}
+          <Text style={styles.default_text}>
+            Note : {film.vote_average} / 10
+          </Text>
+          <Text style={styles.default_text}>
+            Nombre de votes : {film.vote_count}
+          </Text>
+          <Text style={styles.default_text}>
+            Budget : {numeral(film.budget).format('0,0[.]00 $')}
+          </Text>
+          <Text style={styles.default_text}>
+            Genre(s) :{' '}
+            {film.genres
+              .map(function (genre) {
+                return genre.name;
+              })
+              .join(' / ')}
+          </Text>
+          <Text style={styles.default_text}>
+            Companie(s) :{' '}
+            {film.production_companies
+              .map(function (company) {
+                return company.name;
+              })
+              .join(' / ')}
           </Text>
         </ScrollView>
-      )
+      );
     }
   }
 
@@ -161,13 +201,13 @@ class FilmDetail extends React.Component {
 
         {this._displayFloatingActionButton()}
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   main_container: {
-    flex: 1
+    flex: 1,
   },
   loading_container: {
     position: 'absolute',
@@ -176,14 +216,14 @@ const styles = StyleSheet.create({
     top: 0,
     bottom: 0,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   scrollview_container: {
-    flex: 1
+    flex: 1,
   },
   image: {
     height: 169,
-    margin: 5
+    margin: 5,
   },
   title_text: {
     fontWeight: 'bold',
@@ -195,26 +235,26 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     color: '#000000',
-    textAlign: 'center'
+    textAlign: 'center',
   },
   description_text: {
     fontStyle: 'italic',
     color: '#666666',
     margin: 5,
-    marginBottom: 15
+    marginBottom: 15,
   },
-  default_text: {
+  default_text: {
     marginLeft: 5,
     marginRight: 5,
     marginTop: 5,
   },
   favorite_container: {
-    alignItems: 'center'
+    alignItems: 'center',
   },
   favorite_image: {
     flex: 1,
     width: null,
-    height: null
+    height: null,
   },
   share_touchable_floatingactionbutton: {
     position: 'absolute',
@@ -225,21 +265,21 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     backgroundColor: '#e91e63',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   share_image: {
     width: 30,
-    height: 30
+    height: 30,
   },
   share_touchable_headerrightbutton: {
-    marginRight: 8
-  }
-})
+    marginRight: 8,
+  },
+});
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    favoritesFilm: state.favoritesFilm
-  }
-}
+    favoritesFilm: state.favoritesFilm,
+  };
+};
 
-export default connect(mapStateToProps)(FilmDetail)
+export default connect(mapStateToProps)(FilmDetail);
